900535d131ce004124a8403f70d0b40eec984b6b
Astrian <mingriqd@gmail.com>
2018-11-03 20:11:26 +0800

    只有在有同步数据时才会在下载云端数据前清空本地数据

--- storagegetter.js
+++ storagegetter.js
@@ -16,10 +16,13 @@ function exp() { // 导出数据
 
 chrome.storage.sync.get(['iitcdata', 'interval'], res => {
   console.log('ICST Log: storage res: ', res)
-  let impdata = res.iitcdata
-  for (let i in impdata) {
-    if (typeof impdata[i] === 'object') impdata[i] = JSON.stringify(impdata[i])
-    localStorage.setItem(i, impdata[i])
+  if (res.iitcdata){
+    localStorage.clear()
+    let impdata = res.iitcdata
+    for (let i in impdata) {
+      if (typeof impdata[i] === 'object') impdata[i] = JSON.stringify(impdata[i])
+      localStorage.setItem(i, impdata[i])
+    }
   }
   setInterval(() => {
     chrome.storage.sync.set({'iitcdata': exp()}, () => {})
