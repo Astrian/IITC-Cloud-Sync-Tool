37eba10e1217c94c7e402ab0a8ae01d3d4a5a548
Astrian <mingriqd@gmail.com>
2018-11-03 21:07:19 +0800

    简单构建了一下 popup

--- installlistener.js
+++ installlistener.js
@@ -2,6 +2,6 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.get(['interval'], res => {
     if (res.interval) return
     alert('Thanks for installing ICST!')
-    chrome.storage.sync.set({'interval': 30000}, res => {})
+    chrome.storage.sync.set({'interval': 300000}, res => {})
   })
 })
--- popup.js
+++ popup.js
@@ -1,6 +1,26 @@
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    interval: 0
+  },
+  methods: {
+    goToIntel() {
+      chrome.tabs.create({ url: 'https://intel.ingress.com/intel' }, () => {})
+    },
+    justifyInterval(time) {
+      let that = this
+      chrome.storage.sync.set({ interval: time }, res => {
+        that.interval = time
+      })
+    },
+    goToSettings() {
+      console.log('aaa')
+    }
+  },
+  created() {
+    let that = this
+    chrome.storage.sync.get(['interval'], res => {
+      that.interval = res.interval
+    })
   }
 })
